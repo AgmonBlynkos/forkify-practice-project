38f3ee1d5adf2471229b097491d1379e5a7a471d
AgmonBlynkos <agmonblynkos21@gmail.com>
2023-08-28 20:51:48 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '897a3712-6714-4333-a446-7db6d7242f3e';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,66 @@
+import View from './View';
+import icons from 'url:../../img/icons.svg'; // Parcel 2
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+
+      const goToPage = +btn.dataset.goto;
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const currentPage = this._data.page;
+
+    const nextBtn = `
+        <button data-goto="${
+          currentPage + 1
+        }" class="btn--inline pagination__btn--next">
+          <span>Page ${currentPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
+
+    const lastBtn = `
+        <button data-goto="${
+          currentPage - 1
+        }" class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${currentPage - 1}</span>
+        </button>
+      `;
+
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+
+    // Page 1, and there are other pages
+    if (currentPage === 1 && numPages > 1) {
+      return nextBtn;
+    }
+
+    // Last page
+    if (currentPage === numPages && numPages > 1) {
+      return lastBtn;
+    }
+
+    // Other page
+    if (currentPage < numPages) {
+      return `${nextBtn}${lastBtn}`;
+    }
+
+    // Page 1, and there are no more pages
+    return '';
+  }
+}
+
+export default new PaginationView();
